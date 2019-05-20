e95f63e2b51afd0edc98a7798ca08e0f37e50eaa
Jack Linhart <iatenine@users.noreply.github.com>
2019-05-20 01:36:18 -0400

    Version 1.1 update
    https://old.reddit.com/r/20QuestionsMedia/comments/bqhe2e/announcement_follow_notifier_now_in_version_11/

--- followNotifier.py
+++ followNotifier.py
@@ -1,81 +1,132 @@
 import praw
 import time
+import pickle
+import logging
 
-SUBREDDIT = *****                       #Your subreddit name
-NOTIFIER = "AutoModerator"              #Whatever entity delivers the notification messages, most likely Automoderator
-FOLLOW_TEXT = "!follow"                 #The message commenters use to follow a thread (requires automod coordination)
-ERROR_HANDLER = *****                   #Reddit user who will handle all error messages
-SUBSCRIBE_SUBJECT = "Follow Request"    #The modmail subject delivered by notifier when a user subscribes
-DESIRED_FLAIR = "Unsolved"              #Can only subscribe to posts with this flair
-TIME_LIMIT = 900                        #The oldest a post can be in secs and still provide updates
-CURR_TIME = time.time()                 #Establish time at the top to reduce variability caused by long script load times
-AGENT = ******                          #Your user_agent id from Reddit
+#Set to your needs
+SUBREDDIT = ******
+AGENT = *****
 
-reddit = praw.Reddit('bot', user_agent=AGENT)    #Must set 'bot' in lib/python3.6/site-packages/praw.ini
+NOTIFIER = "AutoModerator"
+FOLLOW_TEXT = "!follow"
+SUBSCRIBE_SUBJECT = "Follow Request"
+DESIRED_FLAIR = "Unsolved"
+TIME_LIMIT = 900   #Default for first run only (measured in secs)
+CURR_TIME = time.time()
+TIME_FILE = "time.pickle"
+SUB_FILE = "subscribers.pickle"
+reddit = praw.Reddit('bot', user_agent=AGENT)
 
-def error_catch(e):
-	send_message("Exception caught", e, ERROR_HANDLER)
+#Log for debugging purposes
+CURR_TIME_STR = str(time.strftime("%c"))
+message_count = 0
 
-def send_message(message_subject, message_body, subscriber):
-	try:
-		reddit.redditor(subscriber).message(message_subject, message_body)
-	except Exception as e:
-		error_catch(e)
-		pass
+
+def check_post(post):
+	post = get_post_from_url(post)
+	return post.link_flair_text == DESIRED_FLAIR
+
+def get_last_run():
+	last_run = 0
+	last_time = load(TIME_FILE)
+	save(CURR_TIME, TIME_FILE)
+
+	if last_time == None:
+		return TIME_LIMIT
+	else:
+		last_run = CURR_TIME - last_time
+		return last_run
+
 
 def get_subscribers():
-	strArr = []
+	strArr = load(SUB_FILE)
+	if strArr == None:
+		strArr = []
 	conversations = reddit.subreddit(SUBREDDIT).modmail.conversations(state='all', sort= 'unread', limit=20)
+
 	for conv in conversations:
 		try:
 			if conv.user.name != NOTIFIER and str(conv.subject) != SUBSCRIBE_SUBJECT:
 				continue
 			messagetext = str(conv.messages[0].body_markdown)			
 			if check_post(messagetext.split()[0]) == False:
-				send_message("A thread you were following is no longer valid", messagetext.split()[0] + " is no longer marked unsolved. You have been automatically unsubscribed from further updates", messagetext.split()[1])
-				conv.archive()   #Send message that post has been marked as "solved"
+				send_message("A thread you were following is now marked as Solved", messagetext.split()[0] + " is no longer marked unsolved. You have been automatically unsubscribed from further updates", messagetext.split()[1])
 			else:
 				strArr.append(messagetext.split())
+			conv.archive()
 		except Exception as e:
-			error_catch(e)
+			logging.exception("Exception occurred")
 			pass
+	save(strArr, SUB_FILE)
 	return strArr
 
-#Returns id of a post corresponding to a url passed into the loc argument
-def get_post_from_url(loc):
-	submission = reddit.submission(url = loc)
-	return submission
 
 def get_comments(submission):
 	return submission.comments
 
-def get_forest_str(post, exclude = ""):
+def get_forest_str(post, time_span, exclude = ""):
 	forest = get_comments(get_post_from_url(post))
 	ret = ""
+	src_link = get_post_from_url(post).url
+	print("Checking past " + str(time_span) + " seconds")
 
 	for comment in forest:
 		age = CURR_TIME - comment.created_utc
-		if comment.author == exclude or age > TIME_LIMIT:
+		if comment.author == exclude:
 			continue
 		if comment.author == "Automoderator" or comment.body.lower() == FOLLOW_TEXT.lower():
 			continue
-		ret += ">" + comment.body + "\n"
+		tempStr = ">" + comment.body + "\n"
 		if len(comment.replies.list()) > 0:
+			fire = False
 			for reply in comment.replies.list():
-				ret += ">>" + reply.body + "\n" + "\n"
-	return ret			
+				tempAge = CURR_TIME - reply.created_utc
+				if tempAge < time_span:
+					fire = True 
+				tempStr += ">>" + reply.body + "\n" + "\n"
+			if fire:
+				ret += tempStr
+		elif age < time_span:
+			ret += tempStr
+	if ret != "":
+		ret += "\n\n[" + str(get_post_from_url(post).title) + "](" + str(src_link) + ")" 
+	return ret	
 
-def check_post(post):
-	post = get_post_from_url(post)
-	return post.link_flair_text == DESIRED_FLAIR
+def get_post_from_url(loc):
+	submission = reddit.submission(url = loc)
+	return submission		
+
+def load(FILENAME):
+	try:
+		with open(FILENAME, 'rb') as handle:
+			return pickle.load(handle)
+	except:
+		return None
+
+def notify_subscribers():
+	val = get_subscribers()
+	time_span = get_last_run()
+
+	if len(val) != 0:
+		for i in val:
+			link = i[0]
+			subscriber = i[1]
+			msg = get_forest_str(link, time_span, subscriber)
+			if msg != "":
+				send_message("New comments found in a thread you're following", msg, subscriber)
+
+def save(var, FILENAME):
+	with open(FILENAME, 'wb') as handle:
+		pickle.dump(var, handle)
+
+def send_message(message_subject, message_body, subscriber):
+	try:
+		reddit.redditor(subscriber).message(message_subject, message_body)
+		logging.info('Sent message to %s', subscriber)
+	except Exception as e:
+		logging.exception("Exception occurred")
+		pass
 
 #Main loop begins here, why not
-val = get_subscribers()
-
-if len(val) != 0:
-	for i in val:
-		link = i[0]
-		subscriber = i[1]
-		msg = get_forest_str(link, subscriber)
-		if msg != "":
-			send_message("New comments found in a thread you're following", msg, subscriber)
+logging.basicConfig(level=logging.INFO, filename='20Q_error.log', format='%(asctime)s - %(levelname)s - %(message)s')
+notify_subscribers()
